a0813153b4b08dd355729cf3e85302ea3c290110
Marusero-ju <65613970+Marusero-ju@users.noreply.github.com>
2024-07-01 18:32:44 -0300

    Update and rename Alquiler.js to local.js

--- models/local.js
+++ models/local.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
-const AlquilerSchema = new Schema({
+const LocalSchema = new Schema({
     superficie: {type: Number, required: true},
     habilitado: {type: Boolean, required: true},
     costomes: {type: Boolean, required: true},
     pathimages: {type: String, required: true},
     alquilado: {type: Boolean, required: true},
     promocion:{type: Boolean, required: true},
-    costoMes: { type: Schema.Types.ObjectId, ref: Pago, required: true },
+    costoMes: { type: Number, required: true },
 
 })
 
-module.exports = mongoose.models.Alquiler || mongoose.model('Alquiler', AlquilerSchema);
+module.exports = mongoose.models.Local || mongoose.model('Local', LocalSchema);
