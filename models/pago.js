d298b2a2a44bf5fa9373ddd9c6ad223ac77a5951
gonzanv <glskglskgk@gmail.com>
2024-07-12 08:44:07 -0300

    Se implementa Codigo QR Para pago

--- models/pago.js
+++ models/pago.js
@@ -6,6 +6,7 @@ const PagoSchema = new Schema({
     estado:{type: String, required: true},
     fecha:{type: Date, required: true},
     enlacePago:{type: String, required: true},
+    qrPago:{type: String, required: true},
     usuario:{type: Schema.Types.ObjectId, ref: usuario, required: true}
 })
 module.exports = mongoose.models.Pago || mongoose.model('Pago', PagoSchema);
